7436cf6e1ac6995769bfe4a38ac4255c5ba0fb65
David Miller <racingcow@hotmail.com>
2014-12-14 22:11:05 -0600

    Register in Front End Views

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nodeJwtApp
+ * @description
+ * # nodeJwtApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nodeJwtApp', ['ui.router']);
